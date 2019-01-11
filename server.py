8f82a35043c538caa856dab4fe15c87130f02749
Shouyang Zhou <shouyang@ualberta.ca>
2019-01-11 13:43:02 -0500

    All tests passed

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
 import socketserver
-
+import os
+import mimetypes
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,11 +29,68 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+    ENCODING = "utf-8" # Default Encoding For Things
+    BASEPATH = os.getcwd() + "/www" # Default Content Path For Requests
+
     def handle(self):
+        ''' Handles all incoming requests to the server.
+
+        Decodes requests crudely and handles GET requests, otherwise issues a 405 error.
+        '''
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        request_as_list = self.data.split()
+        request_as_list = [ item.decode("utf-8") for item in request_as_list]
+
+        request_type = request_as_list[0]
+        request_path = request_as_list[1]
+
+        if request_type == "GET":
+            self.GET(request_path)
+        else:
+            self.request.sendError(405)
+
+
+    def GET(self, request_path):
+        ''' Handles GET requests using the request path.
+
+        Defaults to index.html for directories.
+        Sends 404 for paths not found or not starting with the path BASEPATH.
+        '''
+        
+        final_path = self.BASEPATH + request_path
+        print(final_path)
+
+
+        if not os.path.exists(final_path):
+            self.sendError(404)
+            return
+
+        if not os.path.realpath(final_path).startswith(self.BASEPATH):
+            self.sendError(404)
+            return
+
+        if os.path.isdir(final_path):
+            final_path += "index.html"
+
+        content = open(final_path).read()
+
+        self.sendStr(self.generateHttpHeader(200) + self.generateHttpContentType(final_path) + content)
+
+
+    def sendStr(self, string):        
+        byte_string = bytearray(string,self.ENCODING)
+        self.request.sendall(byte_string)
+
+    def sendError(self, num):
+        self.sendStr(self.generateHttpHeader(str(num)))
+
+    def generateHttpHeader(self, code):
+        return "HTTP/1.1 {code} \r\n".format(code = code)
+
+    def generateHttpContentType(self, path):
+        return  "Content-type: {content_type}; \r\n".format(content_type=mimetypes.guess_type(path)[0])
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
